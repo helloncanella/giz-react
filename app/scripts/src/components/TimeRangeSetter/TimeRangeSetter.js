315e7e5e73fc321b5cad073c479b95c70a57f674
Hellon Canella <helloncanella@gmail.com>
2015-11-09 11:48:22 -0200

    TimeRangeSetter's design is ok

--- app/scripts/src/components/TimeRangeSetter/TimeRangeSetter.js
+++ app/scripts/src/components/TimeRangeSetter/TimeRangeSetter.js
@@ -3,17 +3,34 @@ import $ from 'jquery';
 import 'jquery-ui/resizable';
 import 'jquery-ui/draggable';
 
+class TimeRangeSetter extends React.Component {
 
-class TimeRangeSetter extends React .Component {
-
-  componentDidMount(){
+  componentDidMount () {
     $('.TimeRangeSetter').draggable();
+    $('input').keypress(function (e) {
+      var key = String.fromCharCode(e.which);
+
+      if(!((/(\.|\d)/).test(key))){
+        e.preventDefault();
+      }
+    });
   }
 
   render () {
     return (
       <div className='TimeRangeSetter' id={this.props.id}>
-
+        <h5>Set the time interval</h5>
+        <form className='form-inline'>
+          <div className='form-group'>
+            <label htmlFor='start-time'>start time</label>
+            <input className='form-control' id='start-time'></input>
+          </div>
+          <div className='form-group'>
+            <label htmlFor='end-time'>end time</label>
+            <input className='form-control' id='end-time'></input>
+          </div>
+        </form>
+        <button>OK</button>
       </div>
     );
   }
