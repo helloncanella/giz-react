9d9b340e01c96f979c16e0fc211edb13d26fe2e3
Hellon Canella <helloncanella@gmail.com>
2015-11-09 17:12:49 -0200

    moving to flux branch

--- app/scripts/src/components/TimeController/TimeController.js
+++ app/scripts/src/components/TimeController/TimeController.js
@@ -3,14 +3,12 @@ import Button from '../Button/Button';
 import Slider from '../Slider/Slider';
 
 class TimeController extends React.Component {
-  componentDidMount () {
 
-  }
   render () {
     return (
       <div className='TimeController' id={this.props.id}>
         <Button id={'PlayPause'} fontAwesome={'fa fa-play'}/>
-        <Button id={'Stop'} fontAwesome={'fa fa-stop'}/>
+        <Button id={'Stop'} fontAwesome={'fa fa-stop'} />
         <Slider id={'TimeFlow'}/>
       </div>
     );
